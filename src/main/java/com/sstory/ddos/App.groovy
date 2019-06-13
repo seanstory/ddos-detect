69b2b303619da6b09e545233e99933ef0cad876d
storysj <sean.story@digitalreasoning.com>
2019-06-12 19:08:55 -0500

    Added a maven shell with groovy, spock, and slf4j, closing #1

---
+++ src/main/java/com/sstory/ddos/App.groovy
@@ -0,0 +1,15 @@
+package com.sstory.ddos
+
+import groovy.util.logging.Slf4j
+
+/**
+ * Hello world!
+ */
+@Slf4j
+class App
+{
+    static void main( String[] args )
+    {
+        log.info( "Hello World!" )
+    }
+}
